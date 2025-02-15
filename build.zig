c0a0be5e17219ff511588af83d45cbf43d6277c4
Meghan Denny <hello@nektro.net>
2025-02-15 12:52:21 -0800

    add disable_llvm build param

--- build.zig
+++ build.zig
@@ -4,6 +4,7 @@ const Builder = std.Build;
 pub fn build(b: *Builder) void {
     const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
     const target = b.standardTargetOptions(.{});
+    const disable_llvm = b.option(bool, "disable_llvm", "use the non-llvm zig codegen") orelse false;
 
     _ = b.addModule("iguanaTLS", .{
         .root_source_file = b.path("src/main.zig"),
@@ -15,6 +16,8 @@ pub fn build(b: *Builder) void {
         .target = target,
         .optimize = mode,
     });
+    lib.use_llvm = !disable_llvm;
+    lib.use_lld = !disable_llvm;
     b.installArtifact(lib);
 
     const main_tests = b.addTest(.{
@@ -24,6 +27,8 @@ pub fn build(b: *Builder) void {
     main_tests.root_module.addImport("self/files", b.addModule("self/files", .{
         .root_source_file = b.path("files.zig"),
     }));
+    main_tests.use_llvm = !disable_llvm;
+    main_tests.use_lld = !disable_llvm;
 
     const test_step = b.step("test", "Run library tests");
     const test_run = b.addRunArtifact(main_tests);
